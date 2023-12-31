0bd84b099a7a94d1f56ae1ff032ed24521e7556b
Brittney Coble <bwilcutt#gmail.com>
2023-12-31 01:27:29 -0500

    added comments of current tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,7 @@ Problem:
 When users withdraw from an atm, they can typically only withdraw in multiples of 20.
 
 Solution:
+User selects a specific amount or selects from options for withdrawal.
 After specifying an amount to withdraw, users can withdraw in multiples of 1, 5, 10, 20, 50, 100.
 Users can select to withdraw in any combination of bills.
 Users must withdraw the amount of bills that equals the amount withdrawn or cancel the withdrawal.
@@ -24,3 +25,11 @@ bills_selected
 bills_selected_eq_total - boolean
 
 '''
+
+# User selects an amount to withdrawal
+
+# User specifies 100 total to withdraw
+
+# User specifies 100 bill to withdraw
+
+# App limits no other bill type to withdrawal
