415ac1f99102699682106c9e2a0aa2a1c6e93cb9
Brittney Coble <bwilcutt#gmail.com>
2023-12-31 03:07:15 -0500

    added more tests

--- tests/tests.py
+++ tests/tests.py
@@ -66,8 +66,33 @@ def test_hundred_mod_twenty(example_100_wd):
 
 # When user specifies 10 - 10 dollar bill to withdraw, then 100/10 = 10
 def test_hundred_div_ten(example_100_wd):
-    assert example_100_wd//20 == 5
+    assert example_100_wd//10 == 10
 
-# When user specifies 5 - 20 dollar bill to withdraw, then 100 mod 10 = 0, no bills left to select
+# When user specifies 10 - 10 dollar bill to withdraw, then 100 mod 10 = 0, no bills left to select
 def test_hundred_mod_ten(example_100_wd):
     assert example_100_wd % 10 == 0
+
+# When user specifies 100 - 1 dollar bill to withdraw, then 100/1 = 100
+def test_hundred_div_one(example_100_wd):
+    assert example_100_wd//1 == 100
+
+# When user specifies 100 - 1 dollar bill to withdraw, then 100 mod 100 = 0, no bills left to select
+def test_hundred_mod_ten(example_100_wd):
+    assert example_100_wd % 100 == 0
+
+# When user specifies 9 - 10 dollar bill to withdraw, then 100 // 90 = 0, there are bills left to select when remainder
+# is not zero
+def test_hundred_mod_ninety(example_100_wd):
+    assert example_100_wd % 90 != 0
+
+# When user specifies 9 - 10 dollar bill to withdraw, then 100 - 90 = 10; user must select bills = $10 to get to 100 mod 0
+def test_hundred_minus_ninety(example_100_wd):
+    assert example_100_wd - 90 == 10
+
+# When user specifies 9 - 10 dollar bill to withdraw, then 100 - 90 = 10; user must select bills = $10 to get to 100 mod 0
+def test_hundred_minus_ninety_minus_two_fives(example_100_wd):
+    assert example_100_wd - 90 - (5 + 5) == 0
+
+# When user specifies 9 - 10 dollar bill to withdraw, then 100 - 90 = 10; user must select bills = $10 to get to 100 mod 0
+def test_hundred_minus_fifty_minus_twentyplustwtenstwofives(example_100_wd):
+    assert example_100_wd - 50 - (5 + 5)- (10 + 10) - 20 == 0
