1f1a1e521e966ef8c7ddb702de015dd9b4c9d893
Brittney Coble <bwilcutt#gmail.com>
2023-12-30 23:07:27 -0500

    added inputs and outputs, plus main desc to plan and write unit tests

---
+++ tests/tests.py
@@ -0,0 +1,26 @@
+'''
+Problem:
+When users withdraw from an atm, they can typically only withdraw in multiples of 20.
+
+Solution:
+After specifying an amount to withdraw, users can withdraw in multiples of 1, 5, 10, 20, 50, 100.
+Users can select to withdraw in any combination of bills.
+Users must withdraw the amount of bills that equals the amount withdrawn or cancel the withdrawal.
+User can deselect a bill type.
+The amount of bills left will show on screen.
+Bills that cannot be used will be disabled. For example, if withdrawing $100, and only 1 - $20 bill is selected, the option for
+withdrawing a $100 is disabled because there is only $80 in bills left.
+
+Inputs:
+withdrawal_amount
+bill_type
+bill_select
+bill_deselect
+
+Outputs:
+bills_remaining
+bills_unavailable
+bills_selected
+bills_selected_eq_total - boolean
+
+'''
